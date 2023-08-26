fff5181892c6bb6426b6c38d64a6d00349e050cf
Zach Johnson <zgjohnson94@gmail.com>
2023-08-26 09:34:09 -0500

    fetch monsters from api call.

--- src/App.js
+++ src/App.js
@@ -7,25 +7,26 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: "Linda",
-        },
-        {
-          name: "Frank",
-        },
-        {
-          name: "Jackie",
-        },
-      ],
+      monsters: [],
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState(() => {
+        return {monsters: users};
+      },
+      () => {
+        console.log(this.state);
+      }));
   }
 
   render() {
     return (
       <div className="App">
         {this.state.monsters.map((monster) => {
-          return <h1>{monster.name}</h1>;
+          return <h1 key={monster.id}>{monster.name}</h1>;
         })}
       </div>
     );
