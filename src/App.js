5600892e575904b9e0235dca00ed67f5de699ba9
Zach Johnson <zgjohnson94@gmail.com>
2023-08-27 08:17:49 -0500

    Updated filtered monster list

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import { Component } from "react";
 
@@ -8,24 +8,52 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      search: "",
     };
+    console.log("constructor");
   }
 
   componentDidMount() {
+    console.log("componentDidMount");
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((users) => this.setState(() => {
-        return {monsters: users};
-      },
-      () => {
-        console.log(this.state);
-      }));
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
   }
 
   render() {
+    console.log("render");
+
+    const filteredMonsters = this.state.monsters.filter((monster) =>
+      monster.name.toLocaleLowerCase().includes(this.state.search)
+    );
+
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => {
+        <h1>Monsters Rolodex</h1>
+        <input
+          className="search-box"
+          type="text"
+          placeholder="Search Monsters"
+          onChange={(event) => {
+            const search = event.target.value.toLocaleLowerCase();
+            this.setState(() => {
+              return {
+                search,
+              };
+            });
+          }}
+        />
+
+        {filteredMonsters.map((monster) => {
           return <h1 key={monster.id}>{monster.name}</h1>;
         })}
       </div>
