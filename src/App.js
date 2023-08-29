98c69d1f14d35c497febc6293174e5f3ed71e448
Zach Johnson <zgjohnson94@gmail.com>
2023-08-29 18:57:38 -0500

    Made the Card its own component. Finished up the application styling.

--- src/App.js
+++ src/App.js
@@ -43,7 +43,9 @@ class App extends Component {
 
     return (
       <div className="App">
-        <h1>Monsters Rolodex</h1>
+
+        <h1 className="app-title">Monsters Rolodex</h1>
+
         <SearchBox
           onChangeHandler={onSearchChange}
           placeholder="Search Monsters"
